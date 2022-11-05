68fd241191c8e5041a5ca0ff7f311de8a214072b
milosartem@gmail.com <johndoe@example.com>
2022-11-05 16:54:03 +0100

    Refresh

--- src/components/Hooks/useTelegram.js
+++ src/components/Hooks/useTelegram.js
@@ -1,23 +1,19 @@
 const tg = window.Telegram.WebApp;
-
-export function useTelegram (){
-
-    const onClose = () => {
-        tg.close()
-      }
-
-      const onToggleButton = () => {
-       if(tg.MainButton.isVisible){
-        tg.MainButton.show();
-       }else{
-        tg.MainButton.hide();
-       }
-      }
-
-    return{
-        onClose,
-        tg,
-        user: tg.initDataUnsafe?user,
-        onToggleButton,
-    }
+export function useTelegram() {
+const onClose = () => {
+	tg.close()
 }
+const onToggleButton = () => {
+if(tg.MainButton.isVisible) {
+tg.MainButton.show();
+} else {
+ 
+/основная кнопка взаимодействия с ботом/
+}
+	return {
+		onClose,
+		tg,
+		user: tg.initDataUnsafe?.user,
+	
+}
+},
